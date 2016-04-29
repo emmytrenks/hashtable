6458035111048f731dd1eb839a6a65e2b78e44c1
Emily Trenka <eat37@zips.uakron.edu>
2016-04-29 17:31:28 -0400

    Add comments to code

--- hashtable.hpp
+++ hashtable.hpp
@@ -11,22 +11,31 @@ private:
   int count;
   std::vector<std::vector<T>> buckets;
 public:
+  //Create a hash table with 101 initial buckets (a prime number)
   Hashtable() : Hashtable(101) { }
+
   Hashtable(const int &buckets) : count(0) {
     this->buckets.resize(std::max(1, buckets));
   }
 
   void insert(const T t) {
+    //Fetch the correct vector from our buckets
     std::vector<T> &v = buckets[hash(t)];
+    //Check if the value already exists
     if (std::find(v.begin(), v.end(), t) != v.end()) return;
+    //Value does not exist, add it ...
     v.push_back(t);
+    //Rehash if necessary
     if (++count > buckets.size()) rehash();
   }
 
   void remove(const T t) {
+    //Fetch the correct vector from our buckets
     std::vector<T> &v = buckets[hash(t)];
+    //Check if the value exists
     auto pos = std::find(v.begin(), v.end(), t);
     if (pos == v.end()) return;
+    //Erase the value
     v.erase(pos);
     --count;
   }
@@ -36,9 +45,13 @@ public:
 private:
   void rehash() {
     std::vector<T> temp;
+    //Collect all entries in hashtable
     for (auto &v : buckets) for (auto t : v) temp.push_back(t);
+    //Remove them all
     for (auto t : temp) remove(t);
+    //Resize our buckets
     buckets.resize(buckets.size() * 2);
+    //Repopulate hashtable
     for (auto t : temp) insert(t);
   }
 
